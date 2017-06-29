1e8a974212a6a1607bbbca0ea3a36f812296defa
Durvesh Pilankar <durveshpilankar92@gmail.com>
2017-06-29 00:17:57 -0700

    Apple and Orange

---
+++ Algorithms/Implementation/apple_and_orange.py
@@ -0,0 +1,13 @@
+#!/bin/python
+
+import sys
+
+
+s,t = raw_input().strip().split(' ')
+s,t = [int(s),int(t)]
+a,b = raw_input().strip().split(' ')
+a,b = [int(a),int(b)]
+m,n = raw_input().strip().split(' ')
+m,n = [int(m),int(n)]
+apple = map(int,raw_input().strip().split(' '))
+orange = map(int,raw_input().strip().split(' '))
