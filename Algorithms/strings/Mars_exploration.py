0d670a7b709400f2edf86e68d7f20045da8ed0e9
Durvesh Pilankar <durveshpilankar92@gmail.com>
2017-06-28 23:04:11 -0700

    Mars

---
+++ Algorithms/strings/Mars_exploration.py
@@ -0,0 +1,18 @@
+#!/bin/python
+
+import sys
+
+
+S = raw_input().strip()
+count = 0
+i = 0
+while i <= len(S) - 1:
+    if S[i:i+3] != "SOS":
+        if S[i] != "S":
+            count += 1
+        if S[i+1] != "O":
+            count += 1
+        if S[i+2] != "S":
+            count += 1
+    i += 3
+print count
