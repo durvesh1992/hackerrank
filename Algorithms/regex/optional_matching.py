6f9eeb23ee0f727ef8fb7cdd5e0aee87c564dab9
Durvesh Pilankar <durveshpilankar92@gmail.com>
2017-07-08 16:09:33 -0700

    match multiple occurence

--- Algorithms/regex/optional_matching.py
+++ Algorithms/regex/optional_matching.py
@@ -1,11 +1,17 @@
 import re
 
-optionalVal = re.compile(r'Tanmay | Giga(wo)?(man)')
+optionalVal = re.compile(r'Tanmay |Giga(wo)?(man)')
 optionalObj = optionalVal.search("Tanmaya and Gigaman is a good combo")
 
 # will  print gigaman
 print optionalObj.group()
 
 pipeVal = re.compile(r'Giga(wo)?(mon|man|can|dan|fan)')
-pipeObj = pipeVal.search("Gigac-anada delicacy and Gigawomon")
+pipeObj = pipeVal.search("Gigac-wowoanada delicacy and Gigawomon")
 print pipeObj.group()
+
+optionalVal = re.compile(r'Tanmay |Giga(wo)*(man)')
+optionalObj = optionalVal.search("Tanmaya and Gigawowowoman is a good combo")
+
+# will  print gigaman
+print optionalObj.group()
