e1a08f264881e89092cba5877dbc5f02b0fcfc50
Durvesh Pilankar <durveshpilankar92@gmail.com>
2017-07-08 16:26:44 -0700

    non greedy approach

--- Algorithms/regex/greedy_nonGreedy_matching.py
+++ Algorithms/regex/greedy_nonGreedy_matching.py
@@ -10,3 +10,11 @@ print haobj.group()
 
 haobj = haVal.search("hahahahaha")
 print haobj.group()
+
+#Non greedy match
+haVal = re.compile(r'(ha){3,5}?')
+haObj = haVal.search("hahahaha")
+
+#will print only 3 hahaha
+print "Non greedy approac..choose minimum number of 'ha'"
+print haObj.group()
