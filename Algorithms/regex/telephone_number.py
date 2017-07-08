ec58483b5cb9afd9f685e7c86d3e1f19990a5055
Durvesh Pilankar <durveshpilankar92@gmail.com>
2017-07-08 11:53:09 -0700

    Regex for telephone basic

---
+++ Algorithms/regex/telephone_number.py
@@ -0,0 +1,14 @@
+import re
+print "Hello"
+
+phoneNum = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+matchobj = phoneNum.search("My num is 669-264-8414")
+print matchobj.group()
+
+phoneNum = re.compile(r'\d\d\d\d\d\d\d\d\d\d')
+matchobj = phoneNum.search("My num is 6692648414")
+print matchobj.group()
+
+phoneNum = re.compile(r'\d\d\d\d\d\d\d\d\d\d')
+matchobj = phoneNum.search("My num is 6692648417 and 6692648414")
+print matchobj.group()
