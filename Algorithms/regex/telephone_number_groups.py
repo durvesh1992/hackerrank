8d7aa8ae975eaac4a68e50e4e67fbc79e9b73cfc
Durvesh Pilankar <durveshpilankar92@gmail.com>
2017-07-08 12:14:54 -0700

    Parentheses

--- Algorithms/regex/telephone_number_groups.py
+++ Algorithms/regex/telephone_number_groups.py
@@ -11,3 +11,7 @@ print matchobj.groups()
 areaCode, mainNumber = matchobj.groups()
 print "Area Code: ", areaCode
 print "Primary Number: ",mainNumber
+
+phoneNum = re.compile(r'(\(\d\d\d\))-(\d\d\d-\d\d\d\d)')
+matchobj = phoneNum.search("My num is (669)-264-8414")
+print matchobj.groups()
