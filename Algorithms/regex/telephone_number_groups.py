7839255d327fdbe617d02bec4e35cd1f10205c14
Durvesh Pilankar <durveshpilankar92@gmail.com>
2017-07-08 12:03:21 -0700

    Groups added

--- Algorithms/regex/telephone_number_groups.py
+++ Algorithms/regex/telephone_number_groups.py
@@ -4,3 +4,10 @@ matchobj = phoneNum.search("My num is 669-264-8414")
 print matchobj.group()
 print matchobj.group(1)
 print matchobj.group(2)
+
+# Print all the groups
+print matchobj.groups()
+
+areaCode, mainNumber = matchobj.groups()
+print "Area Code: ", areaCode
+print "Primary Number: ",mainNumber
